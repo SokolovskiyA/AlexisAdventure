da08b921bbd0071d03733c1def5b62684550308a
SokolovskiyA <sokolovskiya12@gmail.com>
2023-05-05 22:22:36 -0700

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './assets/logo/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className=' container mx-auto'>
+        <img src={logo} alt="logo" className='w-44 m-4 mx-auto'/>
+        <p>Coming Soon....</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
