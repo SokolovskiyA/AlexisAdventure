fba7a3998e5bdf8e18fd532053964eb65a062718
SokolovskiyA <sokolovskiya12@gmail.com>
2023-05-18 19:58:06 -0700

    started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './assets/logo/logo.png'
+import React from 'react';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+import HomePage from './Pages/HomePage/HomePage.jsx'
+
+
 
 function App() {
   return (
     <div className="App">
-      <div className=' container mx-auto'>
-        <img src={logo} alt="logo" className='w-44 m-4 mx-auto'/>
-        <p>Coming Soon....</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './HomePage.scss'
+import logo from '../../Assets/Images/logo4.png'
+import arrow from '../../Assets/Images/Arrow.png'
+import { Link } from 'react-router-dom'
+
+function HomePage() {
+    return (
+        <div className='page HomePage'>
+            <img className='HomePage__logo' src={logo} alt="logo" />
+            <img className="HomePage__arrow" src={arrow} alt="arrow" />
+            <Link to='/about' className="HomePage__link header"><h1>Discover</h1></Link>
+            <p>At our company, we invite you to discover a new dimension of adventure. With every journey, we aim to redefine the way you explore the great outdoors, empowering you to embrace the beauty of nature while enjoying the comfort and convenience of a home away from home. </p>
+            <h2 className="HomePage__subhead">Shop Now</h2> 
+        </div>
+    )
+}
+
+export default HomePage
