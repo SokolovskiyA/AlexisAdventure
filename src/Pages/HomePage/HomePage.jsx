e5d1dc5e70f5dc9000087c6fff54acc6cd9326b0
SokolovskiyA <sokolovskiya12@gmail.com>
2023-05-19 08:51:16 -0700

    file names

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './HomePage.scss'
-import logo from '../../assets/images/logo.png'
-import arrow from '../../assets/images/Arrow.png'
+import logo from '../../assets/Images/logo.png'
+import arrow from '../../assets/Images/Arrow.png'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
